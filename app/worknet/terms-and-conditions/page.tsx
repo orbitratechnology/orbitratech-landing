ee55d0651907fcd17c07b0be68cbe9be6b4aae5c
Orbitra Tech <orbitra.technology@gmail.com>
2026-09-04 15:53:18 +0530

    refactor: remove TODO comment and instant export.

--- app/worknet/terms-and-conditions/page.tsx
+++ app/worknet/terms-and-conditions/page.tsx
@@ -2,10 +2,6 @@ import type { Metadata } from 'next';
 import Link from 'next/link';
 import { getCopyrightYear } from '@/lib/copyright-year';
 
-// TODO: Cache Components adoption. Refactor this route so this opt-out can be removed.
-// See: https://nextjs.org/docs/app/guides/migrating-to-cache-components
-export const instant = false;
-
 export const metadata: Metadata = {
   title: 'Terms & Conditions | Worknet by Orbitra Tech',
   description:
